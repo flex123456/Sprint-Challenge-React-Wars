060f7e7a231bf407d236654008a28bb9b1f7eda8
milano12345 <dmilano857@gmail.com>
2019-08-16 11:53:35 -0500

    got everything rendered/ adding more styles

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 import axios from 'axios';
-
+import CreateCard from './components/Card'
+import CardData from './components/CardData'
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -9,19 +10,12 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-  const [starData, setStarData] = useState([]);
-  console.log(starData)
-  useEffect(() => {
-  axios
-      .get('https://swapi.co/api/people/')
-      .then(res => setStarData(res.data.results))
-      .catch(err => console.log(err))
-  },[])
 
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <CardData />
     </div>
   );
 }
--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -1,23 +1,27 @@
 import React from 'react'
 import { Card, Icon, Image } from 'semantic-ui-react'
 
-const CardExampleImageCard = () => (
+const CreateCard = (props) => (
   <Card>
-    <Image src='/images/avatar/large/daniel.jpg' wrapped ui={false} />
     <Card.Content>
-      <Card.Header>Daniel</Card.Header>
-      <Card.Meta>Joined in 2016</Card.Meta>
+      <Image href={props.data.homeworld} alt='homeworld'/>
+      <Card.Header>Name: {props.data.name}</Card.Header>
+      <Card.Meta>Birth Year: {props.data.birth_year}</Card.Meta>
       <Card.Description>
-        Daniel is a comedian living in Nashville.
+      <div>Height: {props.data.height}</div> 
+      <div>Hair Color: {props.data.hair_color}</div> 
+      <div>Gender: {props.data.gender} </div>
+      <div>Mass: {props.data.mass}</div>
+      <div>Eye-color: {props.data.eye_color}</div>
+      <div>Skin-color: {props.data.skin_color}</div>
       </Card.Description>
     </Card.Content>
     <Card.Content extra>
       <a>
-        <Icon name='user' />
-        10 Friends
+        <Icon name='star' />
       </a>
     </Card.Content>
   </Card>
 )
 
-export default CardExampleImageCard
+export default CreateCard;
